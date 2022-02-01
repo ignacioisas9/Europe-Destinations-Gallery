32992abc362257119a28e1e3aa3a7ec5f2ca2a6c
Ignacio Isas <ignacioisas8@gmail.com>
2022-02-01 13:58:51 -0300

    footer fixed

--- src/App.js
+++ src/App.js
@@ -13,8 +13,14 @@ function App() {
   return (
     <Router>
       <div className='title'>
+        <br/>
         <h1>Europe destinations</h1>
+        <br/>
       </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <div className='nav'>
 
         {/* Esta seccion de codigos no se que hace. Puede no estar y sigue siendo lo mismo. Falta la imagen principal, el curso debe estar desactualizado */}
